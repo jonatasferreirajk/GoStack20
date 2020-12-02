f2a648b448d5e710affae7bf1a6ca4cab86d06c9
Jonatas Ferreira <silvajonatas2@gmail.com>
2020-12-02 20:17:19 -0300

    Gerando token JWT

--- Backend/src/services/AuthenticateUserService.ts
+++ Backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -27,7 +29,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect email or password combination!');
     }
 
-    return { user };
+    const token = sign({}, 'd068607e78e1da113f8860e8157dc2b1', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
